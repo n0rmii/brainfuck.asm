529b9052e604b698fea00ec226c60f0ecebb9e6a
n0rmii <michal@zywer.pl>
2024-03-06 18:47:37 +0100

    fix loop, add +-<> conds

--- brainfuck.asm
+++ brainfuck.asm
@@ -15,11 +15,33 @@ _start:
 
 		mov r8, instr	;r8 is now iptr
 		mov r9, array	;r9 is now dptr
+		mov r10b, byte [r8]
+
 loop:
+		cmp r10b, 0
+		je exit		;if instruction 0, exit program
+
+		cmp r10b, "+"		;Im using an if-else ladder cuz asm switch-case is black magic
+		jne $+2		;if not +, else it
+		inc byte [r9]
+
+		cmp r10b, "-"
+		jne $+2		;if not -, else it
+		dec byte [r9]
+
+		cmp r10b, "<"
+		jne $+2
+		dec r9
 		
-		cmp r8, 0
-		jne loop		;if instruction not 0, loop back
-		
+		cmp r10b, ">"
+		jne $+2
+		inc r9
+
+		inc r8
+		mov r10b, byte [r8]
+		jmp loop		;if instruction not 0, loop back
+
+exit:
 		mov rax, 60		;sys_exit
 		syscall
 
